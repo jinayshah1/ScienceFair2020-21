f0b794bf2a281ea46b98d3718e500f81f06936c3
jinayshah1 <jinayshah1243@gmail.com>
2021-01-03 13:05:57 -0500

    Changed so that dates are easier to reference and worked on spike

--- src/infectiontopositivetest/DateCountyKey.java
+++ src/infectiontopositivetest/DateCountyKey.java
@@ -1,20 +1,58 @@
 package infectiontopositivetest;
 
 public class DateCountyKey {
-	String caseDate;
+	String caseMonth;
+	String caseDay;
+	String caseYear;
 	String countyCode;
-	
-	public DateCountyKey(String caseDate, String countyCode) {
-		this.caseDate = caseDate;
+
+	public DateCountyKey(String caseMonth, String caseDay, String caseYear, String countyCode) {
+		this.caseMonth = caseMonth;
+		this.caseDay = caseDay;
+		this.caseYear = caseYear;
 		this.countyCode = countyCode;
 	}
 	
-	public String getCaseDate() {
-		return caseDate;
+	public boolean equals(DateCountyKey other) {
+		if (this.caseMonth.equals(other.getCaseMonth()) &&
+			this.caseDay.equals(other.getCaseDay()) &&
+			this.caseYear.equals(other.getCaseYear()) &&
+			this.countyCode.equals(other.getCountyCode())) {
+			return true;
+		}
+		return false;
 	}
 	
 	public String getCountyCode() {
 		return countyCode;
+	}
+
+	public String getCaseMonth() {
+		return caseMonth;
+	}
+
+	public void setCaseMonth(String caseMonth) {
+		this.caseMonth = caseMonth;
+	}
+
+	public String getCaseDay() {
+		return caseDay;
+	}
+
+	public void setCaseDay(String caseDay) {
+		this.caseDay = caseDay;
+	}
+
+	public String getCaseYear() {
+		return caseYear;
+	}
+
+	public void setCaseYear(String caseYear) {
+		this.caseYear = caseYear;
+	}
+
+	public void setCountyCode(String countyCode) {
+		this.countyCode = countyCode;
 	}
 	
 }
